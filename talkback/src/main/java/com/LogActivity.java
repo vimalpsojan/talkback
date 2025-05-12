e00a3acb3f1e3a2dd38b27dff136055f7dcd4f74
Vimal P Sojan <vimalpsojan@gmail.com>
2025-05-12 16:53:29 +0530

    Remove EdgeToEdge.enable from LogActivity

--- talkback/src/main/java/com/LogActivity.java
+++ talkback/src/main/java/com/LogActivity.java
@@ -2,7 +2,6 @@ package com;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -15,7 +14,6 @@ public class LogActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_log);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
